9bfab7c87f25104c680f031b1ba8e638c8516dc1
kento <18120134@student.hcmus.edu.vn>
2020-07-22 18:39:24 +0700

    add data workers

--- dataloaders/shopee_loader.py
+++ dataloaders/shopee_loader.py
@@ -1,5 +1,5 @@
 import torch
 
 
-def shopee_loader(dataset, batch_size, shuffle=False):
-    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+def shopee_loader(dataset, batch_size, shuffle=False, num_workers=0):
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
