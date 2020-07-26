03eabc9ef63689c7398493cff03f4ef403385509
kento <18120134@student.hcmus.edu.vn>
2020-07-26 09:23:45 +0700

    a

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from .sentiment import xlnet_base, xlnet_large, bert_distil, bert_base, bert_large, bert_multi
+from .sentiment import xlnet_base, xlnet_large, bert_mobile, bert_distil, bert_base, bert_large, bert_multi
