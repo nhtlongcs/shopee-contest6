a36ad8663f370eb9c5a75c3032246517cd48c667
kento <18120134@student.hcmus.edu.vn>
2020-08-02 03:19:14 +0700

    sap xong

--- infer_tpu.py
+++ infer_tpu.py
@@ -100,7 +100,10 @@ def predict(config):
         tmp = tmp.argmax(-1).cpu()
         res = torch.cat((res, tmp), 0)
     length = res.shape[0]
-    res = res + torch.Tensor([1]*length).long()
+    if config['model']['args']['args']['nclasses'] == 3:
+        res = res + torch.Tensor([3]*length).long()
+    else:
+        res = res + torch.Tensor([1]*length).long()
 
     res_csv = res.tolist()
     idx = [x+1 for x in range(length)]
